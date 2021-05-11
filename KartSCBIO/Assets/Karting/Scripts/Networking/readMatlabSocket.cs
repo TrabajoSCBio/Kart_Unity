939f8cd322d857c047ab90d687ed2b699aac8fe6
BryanVN1999 <jhovarne@teleco.upv.es>
2021-05-11 17:55:10 +0200

    Matlab script
    Añadido el script de conectarse con matlab

---
+++ KartSCBIO/Assets/Karting/Scripts/Networking/readMatlabSocket.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System;
+using System.IO;
+using System.Text;
+public class readMatlabSocket : MonoBehaviour
+{
+    // Use this for initialization
+    TcpListener listener;
+    String msg;
+    void Start()
+    {
+        listener = new TcpListener(55001);
+        listener.Start();
+        print("is listening");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!listener.Pending())
+        {
+        }
+        else
+        {
+            print("socket comes");
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            msg = reader.ReadToEnd();
+            print(msg);
+        }
+    }
+}
